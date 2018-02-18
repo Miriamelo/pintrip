933ea797676a0fad4a7f9fccf5b4086e37160739
Alynna Alcira <alynna.alcira@gmail.com>
2018-02-17 18:41:59 -0800

    Test fb login

--- js/fb-login.js
+++ js/fb-login.js
@@ -32,3 +32,12 @@ function checkLoginState() {
     console.log(response);
   });
 }
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+}
