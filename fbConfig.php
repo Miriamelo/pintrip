cf69102b1f8344376b0cab805b5926e18d436302
Alynna Alcira <alynna.alcira@gmail.com>
2018-02-17 20:32:05 -0800

    Fb login with db part one lol

---
+++ fbConfig.php
@@ -0,0 +1,46 @@
+<?php
+if(!session_id()){
+    session_start();
+}
+
+// Include the autoloader provided in the SDK
+require_once __DIR__ . '/facebook-php-sdk/autoload.php';
+
+// Include required libraries
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+
+/*
+ * Configuration and setup Facebook SDK
+ */
+$appId         = '399196777217210'; //Facebook App ID
+$appSecret     = 'fb900095bfc512f9e1e9de51bc6c0e48'; //Facebook App Secret
+$redirectURL   = 'http://54.186.123.71/index.html'; //Callback URL
+$fbPermissions = array('email');  //Optional permissions
+
+$fb = new Facebook(array(
+    'app_id' => $appId,
+    'app_secret' => $appSecret,
+    'default_graph_version' => 'v2.2',
+));
+
+// Get redirect login helper
+$helper = $fb->getRedirectLoginHelper();
+
+// Try to get access token
+try {
+    if(isset($_SESSION['facebook_access_token'])){
+        $accessToken = $_SESSION['facebook_access_token'];
+    }else{
+          $accessToken = $helper->getAccessToken();
+    }
+} catch(FacebookResponseException $e) {
+     echo 'Graph returned an error: ' . $e->getMessage();
+      exit;
+} catch(FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
+}
+
+?>
