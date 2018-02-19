821abbc75da7b0edb56d7153aab2581c8a918bd2
Talia Walkey <A00992777@ad.bcit.ca>
2018-02-19 12:01:25 -0800

    changed php

--- addImg_db.php
+++ addImg_db.php
@@ -29,12 +29,12 @@
 
         $sql = "SELECT * FROM Photo";
         
-        $photo = $_POST['photo'];
+        //$photo = $_POST['photo'];
         $coordinates = $_POST['coordinates'];
-        $place_name = $_POST['place_name'];
-        $date = $_POST['date'];
+        //$place_name = $_POST['place_name'];
+        //$date = $_POST['date'];
       
-        $sql = "INSERT INTO Photo (photo, coordinates, place_name, date) VALUES ('$photo', '$coordinates', '$place_name', '$date')";
+        $sql = "INSERT INTO Photo (coordinates) VALUES ('$coordinates')";
         $conn->exec($sql);
              
              
