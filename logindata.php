df422e1c2fd4b8c39e64538da6ad399f9f2c465e
Alynna Alcira <alynna.alcira@gmail.com>
2018-02-19 14:08:35 -0800

    It workssss

--- logindata.php
+++ logindata.php
@@ -8,42 +8,10 @@
 		$email = $_POST['email'];
 		$fb_Id = $_POST['fb_Id'];
     
-        $sql = "INSERT INTO Users (oauth_uid, first_name, email) VALUES ('$fb_Id', '$name', '$score')";    
+        $sql = "INSERT INTO Users (oauth_uid, first_name, email) VALUES ('$fb_Id', '$name', '$email')";    
     
 		$result = mysqli_query($conn , $sql);
     
-		if ($result) {
-			// header("LOCATION: fblogin.php?success");
-			echo "successful entry";
-		}else{
-			echo "not successful";
-		}
-    
         $conn->exec($sql);
-
-//if ($_SERVER['REQUEST_METHOD'] == "POST"){
-//    
-//        $sql = "SELECT * FROM Photos";    
-//    
-//		$name = $_POST['name'];
-//		$email = $_POST['email'];
-//		$fb_Id = $_POST['fb_Id'];
-//    
-//        $sql = "INSERT INTO Users (oauth_uid, first_name, email) VALUES ('$fb_Id', '$name', '$score')";    
-//    
-//		$result = mysqli_query($conn , $sql);
-//    
-//		if ($result) {
-//			// header("LOCATION: fblogin.php?success");
-//			echo "successful entry";
-//		}else{
-//			echo "not successful";
-//		}
-//    
-//        $conn->exec($sql);
-//
-//}else{
-//	echo "Try again Later";
-//}
 
  ?> 
