bf15e617f385ae6cada7774726c9e667e72503da
Talia Walkey <A00992777@ad.bcit.ca>
2018-02-19 11:46:17 -0800

    merged

--- addImg_db.php
+++ addImg_db.php
@@ -1,6 +1,6 @@
 <?php
+/*
         phpinfo();
-
         include_once "database_init.php";
 
         $sql = "SELECT * FROM Photo";
@@ -12,4 +12,35 @@
       
         $sql = "INSERT INTO Photo (photo, coordinates, place_name, date) VALUES ('$photo', '$coordinates', '$place_name', '$date')";
         $conn->exec($sql);
+        */
 ?>
+
+<?php
+    header("Access-Control-Allow-Origin: *");
+    $servername = "http://54.186.123.71";
+    $dblogin = "ec2-user";
+    $password = "";
+    $dbname = "pintrip_db";
+    
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dblogin, $password);
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "SELECT * FROM Photo";
+        
+        $photo = $_POST['photo'];
+        $coordinates = $_POST['coordinates'];
+        $place_name = $_POST['place_name'];
+        $date = $_POST['date'];
+      
+        $sql = "INSERT INTO Photo (photo, coordinates, place_name, date) VALUES ('$photo', '$coordinates', '$place_name', '$date')";
+        $conn->exec($sql);
+             
+             
+    } catch(PDOException $e) {
+        $error = $e->getMessage();
+        echo "<p style='color: red;'>$error</p>";
+    }
+?>
+
