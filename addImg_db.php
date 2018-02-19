6fac4cbd66769beacc92f098c37c102b59d44913
Talia Walkey <a00992777@se14l0124-10.ad.bcit.ca>
2018-02-19 13:25:59 -0800

    removed addImgBase.html

--- addImg_db.php
+++ addImg_db.php
@@ -12,33 +12,3 @@
         $conn->exec($sql);
         
 ?>
-
-<?php/*
-    header("Access-Control-Allow-Origin: *");
-    $servername = "localhost";
-    $dblogin = "ec2-user";
-    $password = "";
-    $dbname = "pintrip_db";
-    
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dblogin, $password);
-
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $sql = "SELECT * FROM Photos";
-        
-        $photo = $_POST['photo'];
-        $coordinates = $_POST['coordinates'];
-        $place_name = $_POST['place_name'];
-        $date = $_POST['date'];
-      
-        $sql = "INSERT INTO Photos (photo, coordinates, place_name, date) VALUES ('$photo', '$coordinates', '$place_name', '$date')";
-        $conn->exec($sql);
-             
-             
-    } catch(PDOException $e) {
-        $error = $e->getMessage();
-        echo "<p style='color: red;'>$error</p>";
-    }*/
-?>
-
