9310965380ca43fd63e590b509cdd4f01449dc9f
Miria <soda.mia@gmail.com>
2018-02-24 14:46:01 -0800

    explore fetch test1

--- explore_gallery.php
+++ explore_gallery.php
@@ -1,4 +1,16 @@
 <?php
 include_once "database_init.php";
-?>
 
+
+// Get images from the database
+$query = $db->query("SELECT * FROM photo ORDER BY date DESC");
+
+if($query->num_rows > 0){
+    while($row = $query->fetch_assoc()){
+        $imageURL = 'uploads/'.$row["img_path"];
+?>
+    <img src="<?php echo $imageURL; ?>" alt="" />
+<?php }
+}else{ ?>
+    <p>No image(s) found...</p>
+<?php } ?>
---
+++ js/explore_gallery.js
@@ -0,0 +1,31 @@
+function loadgallery()
+{
+//    var image = document.getElementById("username").value;
+//        console.log(user);
+//    var data = "user_name=" + user;
+//    var xhr;
+//    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+//        xhr = new XMLHttpRequest();
+//    } else if (window.ActiveXObject) { // IE 8 and older
+//        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+//    }
+    
+//    
+//     xhr.open("POST", "explore_gallery.php", true); 
+//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");                  
+//     xhr.send(data);
+//    
+//	 xhr.onreadystatechange = display_data;
+//    
+//	function display_data() {
+//	 if (xhr.readyState == 4) {
+//      if (xhr.status == 200) {
+//       document.getElementById("testimg").innerHTML = xhr.responseText;
+//          
+//          console.log("Done!");
+//      } else {
+//        alert('There was a problem with the request.');
+//      }
+//     }
+//	}
+//}
