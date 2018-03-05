b4d4692439b36f07210356b90dee680a43493660
Alynna Alcira <alynna.alcira@gmail.com>
2018-03-04 18:53:53 -0800

    Landing page

---
+++ js/landing.js
@@ -0,0 +1,6 @@
+var landing = document.getElementById("landing"),
+    startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", function(){
+   landing.style.display = "none"; 
+});
